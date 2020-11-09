85f1764d329d2797cf214f24a87a8dc05874a316
Delia Constantino <dconstan92@gmail.com>
2020-11-09 19:02:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,11 @@
 require 'pry'
 
 class CashRegister
-<<<<<<< HEAD
   attr_accessor :total, :discount, :items, :last_item_price
-=======
-  attr_accessor :total, :discount, :item
->>>>>>> 5c2ad1ffc10d42dc3dfed27c2b93ab909eb879a6
 
   def initialize(discount = 0.0)
     @total = 0.0
     @discount = discount
-<<<<<<< HEAD
     @items = []
   end
 
@@ -18,15 +13,6 @@ class CashRegister
     quantity.times { @items << item }
     @last_item_price = price * quantity
     self.total += @last_item_price
-=======
-  end
-
-  def add_item(item, price, quantity = 1)
-    @item = item
-    self.items
-    binding.pry
-    self.total += price * quantity
->>>>>>> 5c2ad1ffc10d42dc3dfed27c2b93ab909eb879a6
   end
 
   def apply_discount
@@ -37,14 +23,8 @@ class CashRegister
     end
   end
 
-<<<<<<< HEAD
   def void_last_transaction
     self.total -= @last_item_price
-=======
-  def items
-    items << @item
-    items
->>>>>>> 5c2ad1ffc10d42dc3dfed27c2b93ab909eb879a6
   end
 
 end
